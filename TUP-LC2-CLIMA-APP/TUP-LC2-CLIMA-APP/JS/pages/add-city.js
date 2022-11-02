c11c6b416629c3625cc3c6d26e0d8baa1b15fe7f
Franco <franconahuelr>
2022-11-02 18:00:44 -0300

    update 2/11

--- TUP-LC2-CLIMA-APP/TUP-LC2-CLIMA-APP/JS/pages/add-city.js
+++ TUP-LC2-CLIMA-APP/TUP-LC2-CLIMA-APP/JS/pages/add-city.js
@@ -1,15 +1,48 @@
 const form = document.getElementById("agregar-ciudades");
 
-function getCitiesFromLocalStorage() {
-    let city = document.getElementById("texto-city").value;
+//Uso prevent default para que no se recargue la pagina
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+})
 
-    if (localStorage.getItem("Ciudades") == null) {
+//Obtengo los valores del localStorage
+
+function getCitiesFromLocalStorage() {
+
+    let city = document.getElementById("texto-city").value; //Obtengo el input text de html
+    const result = document.getElementById("result");
+
+    if (localStorage.getItem("Ciudades") == null) { //Verifico si el item Ciudades es nulo, si lo es se crea un array vacio
         localStorage.setItem("Ciudades", "[]");
     }
 
-    let old_cities = JSON.parse(localStorage.getItem("Ciudades"));
+    let old_cities = JSON.parse(localStorage.getItem("Ciudades")); //Guardo el item en una variable y lo parseo
 
-    old_cities.push(city);
+    const ifExist = old_cities.includes(city);  //Creo esta constante para manejar la logica de la verificacion
+
+    result.style.display = "block"; //Al elemento donde se va a mostrar el mensaje del resultado, le agrego display block
+
+    if (!ifExist && city != "") {
+        result.classList.remove('alert-warning');
+        result.classList.remove('alert-danger');
+        old_cities.push(city);                  //Si se cumplen las condiciones del If, pushea los valores de city al array old_cities
+        result.classList.add('alert-success');
+        result.innerText = "Ciudad agregada correctamente";
+    }
+    else if (city == "") {
+        result.classList.remove('alert-warning');
+        result.classList.remove('alert-success');
+        result.classList.add('alert-danger');
+        result.innerText = "Complete los campos";
+    }
+    else if (ifExist) {
+        result.classList.remove('alert-danger');
+        result.classList.remove('alert-success');
+        result.classList.add('alert-warning');
+        result.innerText = "La ciudad ya se encuentra almacenada";
+    }
+
+    //Seteo los valores del local storage
 
     localStorage.setItem("Ciudades", JSON.stringify(old_cities));
 
--- TUP-LC2-CLIMA-APP/TUP-LC2-CLIMA-APP/JS/pages/index.js
+++ TUP-LC2-CLIMA-APP/TUP-LC2-CLIMA-APP/JS/pages/index.js
@@ -1,25 +1,20 @@
 
-
-async function getCity(value) {
+loadSelect();  //primera función a ejecutar 
+var spinner = document.getElementById('spinner'); 
+var charactersDiv = document.getElementById('section-weather-result');
     
-    try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=b54914a753ddd99a46e09a5c522ae648&units=metric&lang=es`);
-        const json = await response.json();
-        console.log(json)
-        return json;
-    } catch (error) {
-        console.log(err)
-    }
-}
+async function getCity(value) {
+    charactersDiv.innerHTML = "";
+    spinner.style.display='block';
 
-
-try {
+    // Esperar que el pedido responda. Buscar y traer info de API.
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=b54914a753ddd99a46e09a5c522ae648&units=metric&lang=es`);
+    //Guardar respuesta API en formato json para manipular info
     const json = await response.json();
+    spinner.style.display='none';
     return json;
-} catch (error) {
-    console.log(err)
 }
+
 
 
 
@@ -28,39 +23,56 @@ function consultar() {
     var value = citySelected.value;
 
     getCity(value)
+        //Esperar ejecución de función getCity
         .then((response) => {
-            console.log(response)
-            showInfo(response)
+            showInfo(response); //Pasa la info de API a la función showInfo y se ejecuta
         })
-        .catch(err => console.log(err))
+        .catch(err =>  alert("Problemas del servidor."));
 
-    console.log(value)
 }
 
 function showInfo(data) {
-    const charactersDiv = document.querySelector('#section-weather-result');
-    charactersDiv.innerHTML = "";
-    const card = document.createElement('div');
-    card.className = 'card';
+    const card = document.createElement('div');  //Crear elemento
+    card.className = 'card';  //Asignar clase
     const title = document.createElement('h3');
-    title.innerText = data.name;
+    title.innerText = data.name;  //Título con nombre de la ciudad
+
+    const img = document.createElement('img');
+    img.src = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";  //Obtengo nombre del icono
 
     const pTemperature = document.createElement('p');
-    pTemperature.innerText = 'Temperatura: ' + data.main.temp + '°C'
+    pTemperature.innerText = 'Temperatura: ' + data.main.temp + '°C';  //Obtengo valor de temp y muestro texto
 
     const pFeelsLike = document.createElement('p');
-    pFeelsLike.innerText = 'Sensación térmica: ' + data.main.feels_like + '°C'
+    pFeelsLike.innerText = 'Sensación térmica: ' + data.main.feels_like + '°C';
 
     const pHumidiy = document.createElement('p');
     pHumidiy.innerText = 'Humedad: ' + data.main.humidity + '%';
 
     const pWindSpeed = document.createElement('p');
-    pWindSpeed.innerText = 'Velocidad del viento: ' + (data.wind.speed*3.6).toFixed(2) + ' km/h'
+    pWindSpeed.innerText = 'Velocidad del viento: ' + (data.wind.speed*3.6).toFixed(2) + ' km/h'; //Se convierte a km/h y trunca en 2 cifras decimales
 
     const pPressure = document.createElement('p');
-    pPressure.innerText = 'Presión: ' + data.main.pressure + ' P'
+    pPressure.innerText = 'Presión: ' + data.main.pressure + ' P';
 
-    card.append(title,pTemperature,pFeelsLike,pHumidiy,pWindSpeed,pPressure);
-    charactersDiv.append(card);
+    card.append(title,img,pTemperature,pFeelsLike,pHumidiy,pWindSpeed,pPressure); //Agrega las const anteriores al div
+    charactersDiv.append(card); //Agrega card a la section-weather-result 
 
+}
+
+function loadSelect(){
+
+    if (localStorage.getItem("Ciudades") == null) {  //Obtiene array Ciudades, si es null, lo crea
+        localStorage.setItem("Ciudades", "[]");      
+    }
+    let cities = JSON.parse(localStorage.getItem("Ciudades"));  //Pasa de texto a json
+    if(cities.length !== 0){
+        var select = document.getElementById('citiesSelect');
+        for (i = 0; i < cities.length; i += 1) {           //crear option según cantidad de ciudades, con valor=nombre de la ciudad
+            option = document.createElement('option');   
+            option.setAttribute('value', cities[i]);     
+            option.appendChild(document.createTextNode(cities[i]));  //crear texto con nombre de ciudad en cada option
+            select.appendChild(option);
+        }
+    }
 }
